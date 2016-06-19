f7113aaf91cbcd4a373400fe446815fca3b1a53e
korvin <korvin424@gmail.com>
2016-06-19 16:06:56 +0300

    rostova

---
+++ appChat.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var port = Number(process.env.PORT || 8008);
+
+var options = {
+//    'log level': 0
+};
+
+
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server, options);
+server.listen(port);
+
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (client) {
+    client.on('message', function (message) {
+        try {
+            client.emit('message', message);
+            client.broadcast.emit('message', message);
+        } catch (e) {
+            console.log(e);
+            client.disconnect();
+        }
+    });
+});
